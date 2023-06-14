c37a4ec36bd1f0c617ced8ac0060d256e410101f
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-06-14 04:22:54 +0300

    Added input for user to enter

--- GuessNumber/Program.cs
+++ GuessNumber/Program.cs
@@ -10,7 +10,18 @@ namespace GuessNumber
             string appVersion = "1.0.0";
             string appAuthor = "Mohamad Kareem";
 
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("{0}: Version {1} by {2}", applicationName, appVersion, appAuthor);
+
+            Console.ResetColor();
+
+            //Ask user name
+            Console.WriteLine("Enter your name:");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, lets start...!!", input);
+
         }
     }
 }
