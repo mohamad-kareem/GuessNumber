9ebbdd3d91faf39dc7081299b9eeb8f94a5ccc30
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-06-14 05:48:44 +0300

    Finalizing my App

--- GuessNumber/Program.cs
+++ GuessNumber/Program.cs
@@ -6,6 +6,55 @@ namespace GuessNumber
     {
         static void Main(string[] args)
         {
+
+            gameinfo();
+            userinfo();
+            while (true)
+            {
+
+            
+            Random random = new Random();
+            int correctNumber=random.Next(1,10);
+            int guess = 0;
+
+            Console.WriteLine("Guess a number between 1 and 10");
+            
+            while (guess != correctNumber)
+            {
+                string input = Console.ReadLine();
+                if(!int.TryParse(input, out guess))
+                {
+                   color(ConsoleColor.Red,"Wrong input!!, please enter a number");
+                    continue;
+                }
+
+                guess = Int32.Parse(input);
+
+                if (guess != correctNumber)
+                {
+                        color(ConsoleColor.Red, "Wrong guess please try again");
+                        
+                }
+            }
+                color(ConsoleColor.Yellow, "Great you are guessed the number");
+              
+
+            Console.WriteLine("Play Again [Y or N]");
+
+            string answer =Console.ReadLine().ToUpper();
+            if(answer == "Y")
+                {
+                    continue;
+                }
+                else if(answer == "N") {
+                    return;
+                }
+               
+            }
+        }
+
+        static void gameinfo()
+        {
             string applicationName = "Number Guesser";
             string appVersion = "1.0.0";
             string appAuthor = "Mohamad Kareem";
@@ -14,14 +63,21 @@ namespace GuessNumber
             Console.WriteLine("{0}: Version {1} by {2}", applicationName, appVersion, appAuthor);
 
             Console.ResetColor();
+        }
 
-            //Ask user name
+        static void userinfo()
+        {
             Console.WriteLine("Enter your name:");
 
-            string input = Console.ReadLine();
+            string Name = Console.ReadLine();
 
-            Console.WriteLine("Hello {0}, lets start...!!", input);
-
+            Console.WriteLine("Hello {0}, lets start...!!", Name);
+        }
+        static void color(ConsoleColor color,string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
