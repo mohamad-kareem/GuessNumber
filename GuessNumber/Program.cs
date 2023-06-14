9c74658896c0865c188ac32486eb947e8711160a
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-06-14 04:12:32 +0300

    Add project files.

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string applicationName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Mohamad Kareem";
+
+            Console.WriteLine("{0}: Version {1} by {2}", applicationName, appVersion, appAuthor);
+        }
+    }
+}
